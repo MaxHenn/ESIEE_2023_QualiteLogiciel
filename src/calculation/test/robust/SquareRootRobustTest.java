3af7eabb64ac9cba24ab67f366364156bf26c6bd
Maxence <maxence.hennekein@orange.fr>
2023-02-27 17:39:11 +0100

    Continuation des tests de calcul
    Test de robutesse pour X fois la fonction
    Clean et vérif de la liste

--- src/calculation/test/robust/SquareRootRobustTest.java
+++ src/calculation/test/robust/SquareRootRobustTest.java
@@ -11,12 +11,21 @@ import calculation.process.CalculationManager;
 public class SquareRootRobustTest {
 
 	private static int default_time_seconds = 1;
-	private static int number_squareroot_test = 500;
+	private static int number_squareroot_test = 1000;
 
 	@Test
 	void testTimeForSquareRoot() {
 		assertTimeout(Duration.ofSeconds(default_time_seconds), () -> {
 			CalculationManager.calculateSquareRoot(1, number_squareroot_test);
+		});
+	}
+
+	@Test
+	void testTimeForMultipleSquareRoot() {
+		assertTimeout(Duration.ofSeconds(default_time_seconds), () -> {
+			for (int it = 0; it < number_squareroot_test; it++) {
+				CalculationManager.calculateSquareRoot(1, 10);
+			}
 		});
 	}
 
--- src/calculation/test/unit/SquareRootUnitTest.java
+++ src/calculation/test/unit/SquareRootUnitTest.java
@@ -3,7 +3,11 @@ package calculation.test.unit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.security.InvalidParameterException;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import calculation.process.CalculationManager;
@@ -12,25 +16,40 @@ public class SquareRootUnitTest {
 
 	private static int default_a = 1;
 	private static int default_b = 5;
+	private static List<Double> listSquareRoot;
+
+	@Before
+	public static void cleanList() {
+		listSquareRoot = new LinkedList<Double>();
+	}
+
+	@After
+	public static void verifyList() {
+		for (Double number : listSquareRoot) {
+			if (number < 0) {
+				fail("A given value is negative");
+			}
+		}
+	}
 
 	@Test
 	void testAIsSuperiorToB() {
 		assertThrows(InvalidParameterException.class, () -> {
-			CalculationManager.calculateSquareRoot(default_a + default_b, default_b);
+			listSquareRoot = CalculationManager.calculateSquareRoot(default_a + default_b, default_b);
 		});
 	}
 
 	@Test
 	void testAIsNegative() {
 		assertThrows(InvalidParameterException.class, () -> {
-			CalculationManager.calculateSquareRoot(-default_a, default_b);
+			listSquareRoot = CalculationManager.calculateSquareRoot(-default_a, default_b);
 		});
 	}
 
 	@Test
 	void testBIsNegative() {
 		assertThrows(InvalidParameterException.class, () -> {
-			CalculationManager.calculateSquareRoot(default_a, -default_b);
+			listSquareRoot = CalculationManager.calculateSquareRoot(default_a, -default_b);
 		});
 	}
 
