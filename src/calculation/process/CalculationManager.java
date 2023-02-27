a79b792f387d7d9641f403c4c47d13e17b007d87
Maxence <maxence.hennekein@orange.fr>
2023-02-27 16:57:57 +0100

    Correction calcul square root
    Les valeurs vont de a à b inclus
    erreur sur la comparaison entre a et b
    ajout de commentaire

--- src/calculation/process/CalculationManager.java
+++ src/calculation/process/CalculationManager.java
@@ -6,20 +6,33 @@ import java.util.List;
 
 public class CalculationManager {
 
+	/**
+	 * Give a List of square root number between a and b
+	 * 
+	 * @param a the starting number. Must be positive and inferior or equal to b.
+	 * @param b the ending number. Must be positive
+	 * @return A List of Double element with represent all square root number
+	 *         between a and b
+	 */
 	public static List<Double> calculateSquareRoot(int a, int b) {
 		if (a < 0 || b < 0) {
 			throw new InvalidParameterException("Parameters must be positive");
-		} else if (a < b) {
+		} else if (a > b) {
 			throw new InvalidParameterException("Parameter a must be lower than b");
 		}
 
-		List<Double> listSquareRoot = new ArrayList<>(b - a);
-		for (int it = a; it < b; it++) {
+		// Initialize the list of square root number of enough size
+		List<Double> listSquareRoot = new ArrayList<>(b - a + 1);
+		for (int it = a; it <= b; it++) {
 			listSquareRoot.add(Math.sqrt(it));
 		}
 		return listSquareRoot;
 	}
 
 	public static void main(String[] args) {
+		List<Double> result = calculateSquareRoot(1, 5);
+		for (Double squareNumber : result) {
+			System.out.println(squareNumber);
+		}
 	}
 }
