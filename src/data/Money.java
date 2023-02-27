efc06bf464528b8eda575b3bee8a030973ffc491
Maxence <maxence.hennekein@orange.fr>
2023-02-27 12:22:48 +0100

    COntinuation de l'implémentation de Money
    Ajout de commentaire aux méthodes
    Ajout de test à la création et de valeur par défaut

--- src/data/Money.java
+++ src/data/Money.java
@@ -1,64 +1,87 @@
 package src.data;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Class to link a amount with a currency
  * 
  * @author MaxHenn
  */
 public class Money {
 
-	private int fAmount;
-	private String fCurrency;
+	private int amount;
+	private String currency;
+
+	private static List<String> currencyType = new ArrayList<String>(Arrays.asList("EUR", "USD", "CHF", "GBP"));
 
 	/**
+	 * Create a new Money instance
 	 * 
 	 * @param amount
 	 * @param currency
 	 */
 	public Money(int amount, String currency) {
-		fAmount = amount;
-		fCurrency = currency;
+		if (amount < 0) {
+			amount = 0;
+		}
+		if (!currencyType.contains(currency)) {
+			currency = currencyType.get(0);
+		}
+		this.amount = amount;
+		this.currency = currency;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * @return the current amount
 	 */
 	public int amount() {
-		return fAmount;
+		return amount;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * @return the currency
 	 */
 	public String currency() {
-		return fCurrency;
+		return currency;
 	}
 
 	/**
+	 * Add into the Money instance the amount of the other Money if the currency are
+	 * equals
 	 * 
-	 * @param m
-	 * @return
+	 * @param m another Money instance to add
+	 * @return This Money Object
 	 */
 	public Money add(Money m) {
 		if (currency().equals(m.currency())) {
-			this.fAmount += m.amount();
+			this.amount += m.amount();
 		}
 		return this;
 	}
 
 	/**
-	 * 
+	 * Add into the Money instance the given amount if the currency is the same.
+	 * Warning : amount can be negative
 	 * @param nAmount
 	 * @param nCurrency
 	 * @return
 	 */
 	public Money add(int nAmount, String nCurrency) {
 		if (currency().equals(nCurrency)) {
-			this.fAmount += nAmount;
+			this.amount += nAmount;
 		}
 		return this;
+	}
 
+	/**
+	 * Check if two Money instance are equals
+	 * @param m The Money to compare
+	 * @return true if both currency and amount are the same
+	 */
+	public boolean equals(Money m) {
+		return (this.currency().equals(m.currency()) && (this.amount() == m.amount()));
 	}
 
 }
